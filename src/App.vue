8a5ad6d6605e89f1a152a2c68362cf33916396df
李杨 <1711467488@qq.com>
2018-04-08 19:12:09 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=1711467488&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:1711467488:53" alt="点击这里给我发消息" title="点击这里给我发消息"/></a>
+    <a
+      target="_blank"
+      href="http://wpa.qq.com/msgrd?v=3&uin=1711467488&site=qq&menu=yes">
+      <img
+        border="0"
+        src="http://wpa.qq.com/pa?p=2:1711467488:53"
+        alt="点击这里给我发消息"
+        title="点击这里给我发消息"/>
+      </a>
   </div>
 </template>
 
