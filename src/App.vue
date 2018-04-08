355e7065ea273ba711f2c09f36ff6bd1d4a3e7de
李杨 <1711467488@qq.com>
2018-04-08 19:21:28 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,10 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
